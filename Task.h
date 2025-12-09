015d2c0a8d6761597c36abfae23c0f8d16181d31
tommymone24 <tommaso.monetti24@gmail.com>
2025-12-09 16:22:09 +0100

    Initial commit

---
+++ Task.h
@@ -0,0 +1,21 @@
+//
+// Created by monet on 04/12/2025.
+//
+
+#ifndef LABPROGRAMMAZIONE_TASK_H
+#define LABPROGRAMMAZIONE_TASK_H
+#include <string>
+using namespace std;
+
+class Task {
+public:
+    string description;
+    string category;
+    string data;
+    bool completed;
+
+    Task(string des, string cat, string d, bool c = false) : description(des), category(cat), data(d), completed(c) {}
+};
+
+
+#endif //LABPROGRAMMAZIONE_TASK_H
---
+++ main.cpp
@@ -0,0 +1,66 @@
+using namespace std;
+#include <iostream>
+#include "Task.h"
+#include "TaskManager.h"
+
+
+int main() {
+    TaskManager taskManager;
+    int scelta;
+
+    // Tentativo di caricare un file all'avvio
+    taskManager.loadFromFile("tasks.txt");
+
+    do {
+        cout << "===== TO DO LIST =====\n";
+        cout << "1. AGGIUNGI TASKS\n";
+        cout << "2. RIMUOVI TASKS\n";
+        cout << "3. COMPLETA TASKS\n";
+        cout << "4. VISUALIZZA TASKS\n";
+        cout << "5. FILTRA PER CATEGORIA\n";
+        cout << "6. SALVA SU FILE\n";
+        cout << "0. ESCI\n";
+        cin >> scelta;
+        cin.ignore();                       // pulisce il buffer
+
+        if (scelta == 1) {
+            std::string titolo, categoria, data;
+
+            std::cout << "Descrizione: ";
+            std::getline(std::cin, titolo);
+            std::cout << "Categoria: ";
+            std::getline(std::cin, categoria);
+            std::cout << "Data (GG-MM-AAAA): ";
+            std::getline(std::cin, data);
+
+            taskManager.addTask(Task(titolo, categoria, data, false));
+            std::cout << "Task aggiunta!\n";
+        }else if (scelta == 2) {
+            int index;
+            cout << "Numero della task da rimuovere: ";
+            cin >> index;
+            taskManager.removeTask(index);
+        }else if (scelta == 3) {
+            int index;
+            cout << "Numero della task da completare: ";
+            cin >> index;
+            taskManager.completeTask(index);
+        }else if (scelta == 4) {
+            taskManager.showTask();
+        }else if (scelta == 5) {
+            string cat;
+            cout << "Categoria da cui filtrare: ";
+            cin.ignore();
+            getline(cin, cat);
+            taskManager.filterByCategory(cat);
+        }else if (scelta == 6) {
+            taskManager.saveToFile("tasks.txt");
+            cout << "Salvato!\n";
+        }
+    }while (scelta != 0);
+
+    // Salvataggio automatico alla chiusura
+    taskManager.saveToFile("tasks.txt");
+
+    return 0;
+}
