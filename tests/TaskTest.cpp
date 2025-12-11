c488d84f9939e0a2bce27a2d2491ee3471de729a
tommymone24 <tommaso.monetti24@gmail.com>
2025-12-11 12:53:38 +0100

    Creazione file test per la classe Task, inizializzato completed a false nel costruttore, creazione file CMakeList.txt nella cartella src

---
+++ tests/TaskTest.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include "../src/Task.h"
+
+TEST(TaskTest, ConstructorInitializesValues) {
+    Task t("Studiare c++", "Studio", "11/12/2025", false);
+
+    EXPECT_EQ(t.description, "Studiare c++");
+    EXPECT_EQ(t.category, "Studio");
+    EXPECT_EQ(t.data, "11/12/2025");
+    EXPECTED_FALSE(t.completed);
+}
